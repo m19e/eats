8633a362dafec1c30a7d7721de7d21fd16b091e6
m19e <nexus.me7290@gmail.com>
2023-02-21 19:38:28 +0900

    refactor: remove commented

--- routes/index.tsx
+++ routes/index.tsx
@@ -14,13 +14,6 @@ export default function Home() {
           class="w-32 h-32"
           alt="the fresh logo: a sliced lemon dripping with juice"
         />
-        {
-          /* <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
-        </p>
-        <Counter start={3} /> */
-        }
         <Builder />
       </div>
     </>
