78bb19ab6f8ceadbcfeffecdf9639be40d6e5890
m19e <nexus.me7290@gmail.com>
2023-02-24 19:28:28 +0900

    refactor: extract signals to utils

--- islands/Builder.tsx
+++ islands/Builder.tsx
@@ -1,50 +1,15 @@
 import type { FunctionComponent } from "preact";
-import { computed, signal } from "@preact/signals";
 import { useEffect, useState } from "preact/hooks";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 
-const COMMAND_IDS = {
-  keywords: "KEYWORDS",
-  exact: "EXACT",
-  or: "OR",
-  minus: "MINUS",
-  tag: "TAG",
-  from: "FROM",
-  to: "TO",
-  until: "UNTIL",
-  since: "SINCE",
-  min_retweets: "MIN_RETWEETS",
-  min_faves: "MIN_FAVES",
-  min_replies: "MIN_REPLIES",
-  "filter:follows": "FILTER:FOLLOWS",
-  "filter:media": "FILTER:MEDIA",
-  "filter:tweet": "FILTER:TWEET",
-} as const;
-type CommandID = keyof typeof COMMAND_IDS;
-type QueryData = {
-  id: CommandID;
-  query: string;
-  active: boolean;
-};
-type UpdateQueryData = Omit<QueryData, "active"> & Partial<QueryData>;
-
-const queryMap = signal(new Map<CommandID, QueryData>());
-const isExcludeUser = signal(true);
-const queryString = computed(() => {
-  return [...queryMap.value.values()].filter((q) => q.active).map((q) =>
-    q.query
-  ).join(" ") + (isExcludeUser.value ? " OR @i -@i" : "");
-});
-const updateQuery = (
-  { id, query, active = true }: UpdateQueryData,
-) => {
-  queryMap.value = new Map(queryMap.value.set(id, { id, query, active }));
-};
-const toggleQuery = (data: QueryData) => {
-  const { id, active } = data;
-  const prevQuery = queryMap.value.get(id) ?? data;
-  updateQuery({ ...prevQuery, active });
-};
+import {
+  isExcludeUser,
+  queryMap,
+  queryString,
+  toggleQuery,
+  updateQuery,
+} from "utils/signals.ts";
+import type { CommandID } from "utils/signals.ts";
 
 type GetQueryFn = (value: string) => string;
 type ContentForm = {
@@ -285,8 +250,6 @@ const contents: Content[] = [
 const Builder = () => {
   return (
     <div class="space-y-3 px-3 w-full">
-      <SearchQuery />
-
       {
         // <Command id="@" title="@" noColon>
         //   <input type="text" placeholder="screen name" class="border px-2" />
@@ -331,6 +294,49 @@ const AppContents = () => {
 
   return <>{body}</>;
 };
+
+const Category = ({ title }: { title: string }) => {
+  return <h2 class="text-xl font-semibold border-b border-black">{title}</h2>;
+};
+
+type CommandProps = {
+  id: CommandID;
+  title?: string;
+  desc?: string;
+  noColon?: boolean;
+  onToggle: (a: boolean) => void;
+};
+
+const Command: FunctionComponent<CommandProps> = (
+  { id, title, desc, noColon = false, onToggle, children },
+) => {
+  return (
+    <div class="flex w-full group">
+      <input
+        type="checkbox"
+        checked={queryMap.value.get(id)?.active}
+        onClick={(e) => onToggle(e.currentTarget.checked)}
+      />
+      <div class="flex flex-1">
+        <div class="flex-1 flex flex-col px-1">
+          <p class="px-1">
+            {title ?? id}
+            {!noColon && ":"}
+            {desc &&
+              (
+                <span class="text-black text-opacity-50 px-1 text-sm">
+                  {desc}
+                </span>
+              )}
+          </p>
+          <span class="bg-gray-500 h-[1px] w-0 group-hover:!w-full transition-all duration-300" />
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
+
 const CommandForm = (props: ContentForm) => {
   if (props.type === "input") {
     const { placeholder, id } = props;
@@ -359,6 +365,72 @@ const CommandForm = (props: ContentForm) => {
     return <Calendar id={props.calendarId} />;
   }
   return null;
+};
+
+type TextInputProps = {
+  placeholder: string;
+  onInput: (value: string) => void;
+  disabled?: boolean;
+};
+const TextInput = ({ placeholder, onInput, disabled }: TextInputProps) => {
+  const [text, setText] = useState("");
+  const handleInput = (value: string) => {
+    onInput(value);
+    setText(value);
+  };
+
+  return (
+    <input
+      class="border px-2 min-w-[12rem]"
+      type="text"
+      placeholder={placeholder}
+      value={text}
+      onInput={(e) => handleInput(e.currentTarget.value)}
+      disabled={!IS_BROWSER || disabled}
+    />
+  );
+};
+
+const tweetFilters: { value: string; label: string }[] = [
+  { value: "nativeretweets", label: "nativeretweets" },
+  { value: "retweets", label: "retweets" },
+  { value: "replies", label: "replies" },
+  { value: "quote", label: "quote" },
+];
+const mediaFilters: { value: string; label: string }[] = [
+  { value: "images", label: "images" },
+  { value: "videos", label: "videos" },
+  { value: "twimg", label: "twimg" },
+  { value: "native_video", label: "native_video" },
+  { value: "media", label: "media" },
+  { value: "consumer_video", label: "consumer_video" },
+  { value: "pro_video", label: "pro_video" },
+  { value: "spaces", label: "spaces" },
+];
+const filtersMap = {
+  tweet: tweetFilters,
+  media: mediaFilters,
+} as const;
+const FilterSelect = ({ type }: { type: "tweet" | "media" }) => {
+  const options = filtersMap[type].map(({ value, label }) => (
+    <option key={value} value={value}>{label}</option>
+  ));
+  const handleChange = (value: string) => {
+    updateQuery({
+      id: `filter:${type}`,
+      query: `filter:${value}`,
+    });
+  };
+
+  return (
+    <select
+      class="border px-2 min-w-[12rem]"
+      defaultValue={filtersMap[type][0].value}
+      onChange={(e) => handleChange(e.currentTarget.value)}
+    >
+      {options}
+    </select>
+  );
 };
 
 function* range(start: number, end: number) {
@@ -426,163 +498,6 @@ const DateSelect = (
     <select
       class="border px-0.5"
       onChange={(e) => onChange(e.currentTarget.value)}
-    >
-      {options}
-    </select>
-  );
-};
-
-const SearchQuery = () => {
-  return (
-    <div class="space-y-1">
-      <div class="flex items-center gap-2">
-        <p class="py-1 px-2 flex-1 rounded border border-gray-400 whitespace-pre-wrap">
-          {queryString.value || (
-            <span class="opacity-50">
-              Search Query
-            </span>
-          )}
-        </p>
-        <a
-          class="flex justify-center items-center rounded-full border-2 border-gray-400 w-8 h-8"
-          href={`https://twitter.com/search?q=${
-            encodeURIComponent(queryString.value)
-          }&src=typed_query`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            class="w-5 h-5"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </a>
-      </div>
-      <div class="flex items-center gap-2">
-        <input
-          type="checkbox"
-          checked={isExcludeUser.value}
-          onClick={(e) => isExcludeUser.value = !isExcludeUser.value}
-        />
-        <p class="text-sm">
-          Exclude user name and screen name
-        </p>
-      </div>
-    </div>
-  );
-};
-
-const Category = ({ title }: { title: string }) => {
-  return <h2 class="text-xl font-semibold border-b border-black">{title}</h2>;
-};
-
-type TextInputProps = {
-  placeholder: string;
-  onInput: (value: string) => void;
-  disabled?: boolean;
-};
-
-const TextInput = ({ placeholder, onInput, disabled }: TextInputProps) => {
-  const [text, setText] = useState("");
-  const handleInput = (value: string) => {
-    onInput(value);
-    setText(value);
-  };
-
-  return (
-    <input
-      class="border px-2 min-w-[12rem]"
-      type="text"
-      placeholder={placeholder}
-      value={text}
-      onInput={(e) => handleInput(e.currentTarget.value)}
-      disabled={!IS_BROWSER || disabled}
-    />
-  );
-};
-
-type CommandProps = {
-  id: CommandID;
-  title?: string;
-  desc?: string;
-  noColon?: boolean;
-  onToggle: (a: boolean) => void;
-};
-
-const Command: FunctionComponent<CommandProps> = (
-  { id, title, desc, noColon = false, onToggle, children },
-) => {
-  return (
-    <div class="flex w-full group">
-      <input
-        type="checkbox"
-        checked={queryMap.value.get(id)?.active}
-        onClick={(e) => onToggle(e.currentTarget.checked)}
-      />
-      <div class="flex flex-1">
-        <div class="flex-1 flex flex-col px-1">
-          <p class="px-1">
-            {title ?? id}
-            {!noColon && ":"}
-            {desc &&
-              (
-                <span class="text-black text-opacity-50 px-1 text-sm">
-                  {desc}
-                </span>
-              )}
-          </p>
-          <span class="bg-gray-500 h-[1px] w-0 group-hover:!w-full transition-all duration-300" />
-        </div>
-        {children}
-      </div>
-    </div>
-  );
-};
-
-const tweetFilters: { value: string; label: string }[] = [
-  { value: "nativeretweets", label: "nativeretweets" },
-  { value: "retweets", label: "retweets" },
-  { value: "replies", label: "replies" },
-  { value: "quote", label: "quote" },
-];
-const mediaFilters: { value: string; label: string }[] = [
-  { value: "images", label: "images" },
-  { value: "videos", label: "videos" },
-  { value: "twimg", label: "twimg" },
-  { value: "native_video", label: "native_video" },
-  { value: "media", label: "media" },
-  { value: "consumer_video", label: "consumer_video" },
-  { value: "pro_video", label: "pro_video" },
-  { value: "spaces", label: "spaces" },
-];
-const filtersMap = {
-  tweet: tweetFilters,
-  media: mediaFilters,
-} as const;
-
-const FilterSelect = ({ type }: { type: "tweet" | "media" }) => {
-  const options = filtersMap[type].map(({ value, label }) => (
-    <option key={value} value={value}>{label}</option>
-  ));
-  const handleChange = (value: string) => {
-    updateQuery({
-      id: `filter:${type}`,
-      query: `filter:${value}`,
-    });
-  };
-
-  return (
-    <select
-      class="border px-2 min-w-[12rem]"
-      defaultValue={filtersMap[type][0].value}
-      onChange={(e) => handleChange(e.currentTarget.value)}
     >
       {options}
     </select>
