b2d89d2f0a15114befebe6683f7a90641c6ecb80
m19e <nexus.me7290@gmail.com>
2023-02-26 22:33:04 +0900

    style: twitter colored search-query

--- routes/index.tsx
+++ routes/index.tsx
@@ -9,11 +9,13 @@ export default function Home() {
         <title>EATS | Easy Advanced Twitter Search</title>
       </Head>
       <div class="p-4 mx-auto flex flex-col items-center max-w-screen-sm">
-        <img
+        {
+          /* <img
           src="/logo.svg"
           class="w-32 h-32"
           alt="the fresh logo: a sliced lemon dripping with juice"
-        />
+        /> */
+        }
         <div class="space-y-3 px-3 w-full">
           <SearchQuery />
           <QueryBuilder />
