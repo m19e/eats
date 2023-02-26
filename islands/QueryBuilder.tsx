466d15a3cb32048b54604d06a67c7fb819d5b198
m19e <nexus.me7290@gmail.com>
2023-02-26 23:04:43 +0900

    style: remove border from category

--- islands/QueryBuilder.tsx
+++ islands/QueryBuilder.tsx
@@ -288,7 +288,7 @@ const AppContents = () => {
 };
 
 const Category = ({ title }: { title: string }) => {
-  return <h2 class="text-xl font-semibold border-b border-black">{title}</h2>;
+  return <h2 class="text-xl font-semibold">{title}</h2>;
 };
 
 type CommandProps = {
