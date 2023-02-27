a8479758237eb2206feea7ff93b16d8679b7b834
m19e <nexus.me7290@gmail.com>
2023-02-27 23:14:47 +0900

    chore: updated fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,8 +4,9 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
+import * as $1 from "./routes/_app.tsx";
+import * as $2 from "./routes/api/joke.ts";
+import * as $3 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/QueryBuilder.tsx";
 import * as $$2 from "./islands/SearchQuery.tsx";
@@ -13,8 +14,9 @@ import * as $$2 from "./islands/SearchQuery.tsx";
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/_app.tsx": $1,
+    "./routes/api/joke.ts": $2,
+    "./routes/index.tsx": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
