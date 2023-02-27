d0c2e7f22a7b436ec3e02d52270bf800c7b7bea5
m19e <nexus.me7290@gmail.com>
2023-02-27 23:15:55 +0900

    feat: remove head in routes/index

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,23 +4,18 @@ import QueryBuilder from "islands/QueryBuilder.tsx";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>EATS | Easy Advanced Twitter Search</title>
-      </Head>
-      <div class="p-4 mx-auto flex flex-col items-center max-w-screen-sm">
-        {
-          /* <img
+    <div class="p-4 mx-auto flex flex-col items-center max-w-screen-sm">
+      {
+        /* <img
           src="/logo.svg"
           class="w-32 h-32"
           alt="the fresh logo: a sliced lemon dripping with juice"
         /> */
-        }
-        <div class="space-y-3 w-full">
-          <SearchQuery />
-          <QueryBuilder />
-        </div>
+      }
+      <div class="space-y-3 w-full">
+        <SearchQuery />
+        <QueryBuilder />
       </div>
-    </>
+    </div>
   );
 }
