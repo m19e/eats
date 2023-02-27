78bc330faee8953076c5810e12b0dba6d300d10a
m19e <nexus.me7290@gmail.com>
2023-02-27 18:27:36 +0900

    style: decorate checkbox

--- islands/SearchQuery.tsx
+++ islands/SearchQuery.tsx
@@ -36,13 +36,30 @@ const SearchQuery = () => {
         </div>
       </a>
 
-      <div class="flex items-center gap-2">
-        <input
-          type="checkbox"
-          checked={isExcludeUser.value}
-          onClick={() => isExcludeUser.value = !isExcludeUser.value}
-        />
-        <p class="text-sm">
+      <div
+        class="flex items-center gap-2 cursor-pointer"
+        onClick={() => isExcludeUser.value = !isExcludeUser.value}
+      >
+        <div
+          class={`flex items-center justify-center w-5 h-5 rounded-full ${
+            isExcludeUser.value ? "bg(twitter)" : "border-2 border-gray-600"
+          }`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-3.5 h-3.5 text-white"
+          >
+            <path
+              fillRule="evenodd"
+              d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+
+        <p class="text-sm select-none">
           Exclude user name and screen name
         </p>
       </div>
