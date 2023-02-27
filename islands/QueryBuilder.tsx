02a2559519459eb4f157502f8889b66625e27752
m19e <nexus.me7290@gmail.com>
2023-02-27 18:44:21 +0900

    feat: implement command checkbox

--- islands/QueryBuilder.tsx
+++ islands/QueryBuilder.tsx
@@ -304,10 +304,9 @@ const Command = (
 ) => {
   return (
     <div class="flex w-full group">
-      <input
-        type="checkbox"
+      <Checkbox
         checked={queryMap.value.get(id)?.active}
-        onClick={(e) => onToggle(e.currentTarget.checked)}
+        onClick={(checked) => onToggle(checked)}
       />
       <div class="flex flex-1">
         <div class="flex-1 flex flex-col px-1">
@@ -325,6 +324,35 @@ const Command = (
         </div>
         <CommandForm {...form} />
       </div>
+    </div>
+  );
+};
+
+const Checkbox = (
+  { checked, onClick }: {
+    checked?: boolean;
+    onClick: (checked: boolean) => void;
+  },
+) => {
+  return (
+    <div
+      class={`flex items-center justify-center w-5 h-5 rounded-full cursor-pointer ${
+        checked ? "bg(twitter)" : "border-2 border-gray-600"
+      }`}
+      onClick={() => onClick(!checked)}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="w-3.5 h-3.5 text-white"
+      >
+        <path
+          fillRule="evenodd"
+          d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+          clipRule="evenodd"
+        />
+      </svg>
     </div>
   );
 };
