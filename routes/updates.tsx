0e437deaaf235227d9b5514c9de46f2a3145aa43
m19e <nexus.me7290@gmail.com>
2023-03-09 19:06:45 +0900

    refactor: set class at div html dangerrously set

--- routes/updates.tsx
+++ routes/updates.tsx
@@ -27,11 +27,8 @@ const Updates = ({ data: { markdown } }: PageProps<Data>) => {
       <div class="flex flex-col min-h-screen">
         <Header />
         <main class="flex items-start justify-center flex-1 bg-gray-100">
-          <div class="m-2 sm:m-4 p-8 w-full max-w-screen-sm rounded-lg bg-white">
-            <Body markdown={markdown} />
-          </div>
+          <Body markdown={markdown} />
         </main>
-
         <Footer />
       </div>
     </>
@@ -47,6 +44,7 @@ const Body = ({ markdown }: { markdown: string }) => {
 
   return (
     <div
+      class="m-2 sm:m-4 p-8 w-full max-w-screen-sm rounded-lg bg-white"
       dangerouslySetInnerHTML={{ __html: html }}
     />
   );
