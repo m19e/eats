1f8ec3a55505bc9f04a60c0bc2d4a740449e189f
m19e <nexus.me7290@gmail.com>
2023-03-12 20:53:27 +0900

    refactor: extract command/checkbox

--- components/Command/index.tsx
+++ components/Command/index.tsx
@@ -4,6 +4,7 @@ import { motion, useAnimationControls } from "framer-motion";
 import type { CommandData } from "/types/builder.ts";
 import { focusedCommand, queryMap, toggleQuery } from "/utils/signals.ts";
 
+import { Checkbox } from "/components/Command/Checkbox.tsx";
 import { Label } from "/components/Command/Label.tsx";
 import { Form } from "/components/Command/Form.tsx";
 import { Hint } from "/components/Command/Hint.tsx";
@@ -57,35 +58,6 @@ export const Command = (
       >
         <Hint hint={hint} />
       </motion.div>
-    </div>
-  );
-};
-
-type CheckBoxProps = {
-  checked?: boolean;
-  onClick: (checked: boolean) => void;
-};
-
-const Checkbox = ({ checked, onClick }: CheckBoxProps) => {
-  return (
-    <div
-      class={`flex items-center justify-center w-5 h-5 rounded-full cursor-pointer ${
-        checked ? "bg(twitter)" : "border-2 border-gray-600"
-      }`}
-      onClick={() => onClick(!checked)}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="w-3.5 h-3.5 text-white"
-      >
-        <path
-          fillRule="evenodd"
-          d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-          clipRule="evenodd"
-        />
-      </svg>
     </div>
   );
 };
