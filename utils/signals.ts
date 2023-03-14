e91c8f9f5ce19f6cb15de56bc29e9a28db78e865
m19e <nexus.me7290@gmail.com>
2023-03-14 15:57:06 +0900

    feat: set default query active on query list

--- utils/signals.ts
+++ utils/signals.ts
@@ -9,18 +9,29 @@ type QueryData = {
 };
 type UpdateQueryData = Omit<QueryData, "active"> & Partial<QueryData>;
 
-const defaultQueries: QueryData[] = [
-  { id: "keywords", query: "", active: false },
-  { id: "exact", query: `""`, active: false },
-  { id: "or", query: "", active: false },
-  { id: "minus", query: "-", active: false },
-  { id: "tag", query: "#", active: false },
+const defaultQueries: UpdateQueryData[] = [
+  { id: "keywords", query: "" },
+  { id: "exact", query: `""` },
+  { id: "or", query: "" },
+  { id: "minus", query: "-" },
+  { id: "tag", query: "#" },
   { id: "exclude_name", query: "OR @i -@i", active: true },
+  { id: "from", query: "from:" },
+  { id: "to", query: "to" },
+  { id: "filter:follows", query: "filter:follows" },
+  { id: "filter:media", query: "filter:images" },
+  { id: "filter:tweet", query: "filter:nativeretweets" },
+  { id: "lang", query: "lang:ja" },
+  { id: "until", query: "until:2023-1-1" },
+  { id: "since", query: "since:2023-1-1" },
+  { id: "min_retweets", query: "min_retweets:0" },
+  { id: "min_faves", query: "min_faves:0" },
+  { id: "min_replies", query: "min_replies:0" },
 ];
 
 const createQueryTuple = (
-  data: QueryData,
-): [CommandID, QueryData] => [data.id, data];
+  data: UpdateQueryData,
+): [CommandID, QueryData] => [data.id, { active: false, ...data }];
 
 export const queryMap = signal(
   new Map<CommandID, QueryData>(defaultQueries.map(createQueryTuple)),
